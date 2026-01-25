150b749c9339f07df857070b426e15c49b827810
NoCapCode <nocapcode.official@gmail.com>
2026-01-25 17:09:21 +0530

    Update CaseStudyDetail.js

--- src/components/CaseStudyDetail.js
+++ src/components/CaseStudyDetail.js
@@ -176,7 +176,7 @@ const CaseStudyDetail = () => {
             <div className={styles.socials}>
               <span><a href="https://www.linkedin.com/company/nocapcode"  rel="noreferrer" target="_blank"><Linkedin size={16} color="rgba(190, 190, 190, 1)"/></a></span>
                <span onClick={()=>{navigate("/404")}}><FontAwesomeIcon icon={faXTwitter} /></span>
-              <span onClick={()=>{navigate("/404")}}><Instagram size={16} color="rgba(190, 190, 190, 1)"/></span>
+              <span><a href="https://www.instagram.com/nocapcode.cloud" target="_blank" rel="noreferrer"><Instagram size={16} color="rgba(190, 190, 190, 1)" /></a></span>
               
               
             </div>
