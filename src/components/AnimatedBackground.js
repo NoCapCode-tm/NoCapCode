a2eb15f7378010a7916270446f20e3be0e3206a2
ziyamohammad <mohammad2311061@akgec.ac.in>
2026-02-21 03:14:19 +0530

    video optimized

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -1005,15 +1005,8 @@ const page4TextLines = [
     <>
     <div className={styles.container}>
       {/* Background */}
-     <video
-  className={styles.videoBg}
-  autoPlay
-  loop
-  muted
-  playsInline
-  preload="none"
-  poster="/hero.webp"
->
+     <video autoplay muted loop playsinline preload="none" poster="/hero.webp">
+  <source src="/herosection.webm" type="video/webm" />
   <source src="/bg1.mp4" type="video/mp4" />
 </video>
       {/* Content */}
