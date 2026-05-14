dffb4f023f7a48f9f36ca2eb5ace55071b4910e4
Om Vashishtha <omvashishtha0412@gmail.com>
2026-05-14 21:08:34 +0530

    removed onboarding-page, login-page, app.js import and route updated

--- src/App.js
+++ src/App.js
@@ -14,16 +14,6 @@ import Privacy from "./components/Privacy";
 import ScrollToTop from "./components/ScrollToTop";
 import CareersPage from "./components/CareersPage";
 import ClarityForm from "./components/ClarityForm";
-import Onboarding from "./components/Onboarding";
-import Step1 from "./components/onboarding/Step1";
-import Step2 from "./components/onboarding/Step2";
-import Step3 from "./components/onboarding/Step3";
-import Step4 from "./components/onboarding/Step4";
-import Step5 from "./components/onboarding/Step5";
-import Step6 from "./components/onboarding/Step6";
-import Step7 from "./components/onboarding/Step7";
-import OnboardingComplete from "./components/OnboardingComplete";
-import Login from "./components/Login"
 import AddCaseStudy from "./components/AddCaseStudy";
 import Addjobposting from "./components/JobPosting";
 import AdminLogin from "./components/AdminLogin";
@@ -83,20 +73,10 @@ function AppWrapper() {
             <Route path="/addjobposting" element={<Addjobposting/>} />
             <Route path="/career/:id/applicationform" element={<Applicationform/>} />
             <Route path="/clarity" element={<ClarityForm/>} />
-            <Route path="/login" element={<Login />} />
             <Route path="/verify" element={<Verify />} />
             <Route path="/certificate" element={<Certificate />} />
             <Route path="/adminlogin" element={<AdminLogin />} />
             <Route path="/adminhome" element={<Home/>} />
-            <Route path="/onboarding" element={ <Onboarding /> } />
-            <Route path="/onboarding/step1" element={<Step1 />} />
-            <Route path="/onboarding/step2" element={<Step2 />} />
-            <Route path="/onboarding/step3" element={<Step3 />} />
-            <Route path="/onboarding/step4" element={<Step4 />} />
-            <Route path="/onboarding/step5" element={<Step5 />} />
-            <Route path="/onboarding/step6" element={<Step6 />} />
-            <Route path="/onboarding/step7" element={<Step7 />} />
-            <Route path="/onboarding/complete" element={<OnboardingComplete />} />
             <Route path="*" element={<NotFoundPage setNotFound={setNotFound} />} />
           </Routes>
 
